741f427200a042905160505e7950451e8eb745ee
Davide Ghinato <dghinato@gmail.com>
2015-05-07 16:44:16 +0200

    Piccole modifiche. M_M

--- Elementary_Function.py
+++ Elementary_Function.py
@@ -65,12 +65,20 @@ def generateLargePrime(k):
      #return "Failure after "+`r_` + " tries."
      return False
 
-def GetPrimeNumber():
-    nbits = Util.NBITS
+def getPrimeNumber(nbits):
     p = generateLargePrime(nbits)
     while not p:
         p = generateLargePrime(nbits)
     return p
+
+def egcd(a, b):
+    x,y, u,v = 0,1, 1,0
+    while a != 0:
+        q, r = b//a, b%a
+        m, n = x-u*q, y-v*q
+        b,a, x,y, u,v = a,r, u,v, m,n
+    gcd = b
+    return gcd, x, y
 
 if __name__ == "__main__":
     
