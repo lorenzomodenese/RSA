3ce84d6f812a76e84b6239ad4b7ae2ae8738324d
Luca Gentilini <luca.gentilini@student.unife.it>
2015-05-08 10:26:39 +0200

    7
    7

--- main_Client_Sender.py
+++ main_Client_Sender.py
@@ -13,10 +13,27 @@ def RiceviKeys():
 
     return n,e
 
+def encode():
+    bytes = bitarray()
+    data = bitarray()
+    output = bitarray()
+
+    with open (Util.inputFile, "rb") as f:
+        bytes.fromfile(f)
+    print "Data length:", ((bytes.length()/8)/1024), "KB\n"
+
+    for i in range(bytes.length()%64):
+        bytes.extend("0")
+
+    for i in range(bytes.length()/64):
+        data = bytes[(0+(64*i)):(64+(64*i))]
+
+
 
 if __name__ == "__main__":
     n,e=RiceviKeys()
     print "  -> Encryption Key: ",e
     print "  -> N: ",n
+    print str(ord(n))
 
 
